6cb7dfa7f0e641be0ad2b72b6c33771fc236b37c
Ayla Pessoa <aylamargarida21@gmail.com>
2026-06-29 21:39:39 -0300

    Adiciona mensagem de boas vindas

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+window.onload = fuction() {
+    alert("Bem-Vindo ao site full stack noite!");
+};
 function mostrarMensagem() {
     document.getElementById("mensagem").innerHTML = 
         "Parabéns! Seu projeto está funcionando corretamente.";
